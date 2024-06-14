beb07b2f8ae16ccb008ca00670a55225ef16a384
miro <jarbasai@mailfence.com>
2024-06-14 21:40:57 +0100

    fix/image_time maybe

--- __init__.py
+++ __init__.py
@@ -10,35 +10,32 @@ class ConfuciusQuotesSkill(OVOSSkill):
     def show_confucius(self, caption):
         self.gui.show_image(join(dirname(__file__), "ui", "confucius.jpg"),
                             caption=caption,
+                            override_idle=10,
+                            override_animations=True,
                             fill='PreserveAspectFit')
 
-    @intent_handler(
-        IntentBuilder("ConfuciusQuote").require('confucius').require('quote'))
+    @intent_handler(IntentBuilder("ConfuciusQuote").require('confucius').require('quote'))
     def handle_quote(self, message):
         utterance = self.dialog_renderer.render("quote", {})
         self.show_confucius(utterance)
         self.speak(utterance, wait=True)
         self.gui.release()
 
-    @intent_handler(
-        IntentBuilder("ConfuciusLive").require('confucius').require(
-            'when').require('live'))
+    @intent_handler(IntentBuilder("ConfuciusLive").require('confucius').require('when').require('live'))
     def handle_live(self, message):
         utterance = self.dialog_renderer.render("live", {})
         self.show_confucius(utterance)
         self.speak(utterance, wait=True)
         self.gui.release()
 
-    @intent_handler(
-        IntentBuilder("ConfuciusBirth").require('confucius').require('birth'))
+    @intent_handler(IntentBuilder("ConfuciusBirth").require('confucius').require('birth'))
     def handle_birth(self, message):
         utterance = self.dialog_renderer.render("birth", {})
         self.show_confucius(utterance)
         self.speak(utterance, wait=True)
         self.gui.release()
 
-    @intent_handler(
-        IntentBuilder("ConfuciusDeath").require('confucius').require('death'))
+    @intent_handler(IntentBuilder("ConfuciusDeath").require('confucius').require('death'))
     def handle_death(self, message):
         utterance = self.dialog_renderer.render("death", {})
         self.show_confucius(utterance)
