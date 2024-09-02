f9512b05f7686ef372157cb8ed4b913dd8bb5b20
JarbasAl <JarbasAl@users.noreply.github.com>
2024-09-02 23:15:47 +0000

    Prepare Next Version

---
+++ build/lib/ovos_skill_confucius_quotes/README.md
@@ -0,0 +1,24 @@
+# <img src='./confucius.png' width='50' height='50' style='vertical-align:bottom'/> Confucius Quotes
+
+
+## About
+
+Quote from Confucius
+  
+![](gui.png)
+
+## Examples
+* "Who is Confucius"
+* "Quote from Confucius"
+* "When was Confucius born"
+* "When did Confucius die"
+
+
+## Credits
+JarbasAl
+
+## Category
+**Information**
+
+## Tags
+#quotes
---
+++ build/lib/ovos_skill_confucius_quotes/__init__.py
@@ -0,0 +1,50 @@
+from os.path import join, dirname
+
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.intents import IntentBuilder
+from ovos_workshop.skills import OVOSSkill
+
+
+class ConfuciusQuotesSkill(OVOSSkill):
+
+    def show_confucius(self, caption):
+        self.gui.show_image(join(dirname(__file__), "ui", "confucius.jpg"),
+                            caption=caption,
+                            override_idle=10,
+                            override_animations=True,
+                            fill='PreserveAspectFit')
+
+    @intent_handler(IntentBuilder("ConfuciusQuote").require('confucius').require('quote'))
+    def handle_quote(self, message):
+        utterance = self.dialog_renderer.render("quote", {})
+        self.show_confucius(utterance)
+        self.speak(utterance, wait=True)
+        self.gui.release()
+
+    @intent_handler(IntentBuilder("ConfuciusLive").require('confucius').require('when').require('live'))
+    def handle_live(self, message):
+        utterance = self.dialog_renderer.render("live", {})
+        self.show_confucius(utterance)
+        self.speak(utterance, wait=True)
+        self.gui.release()
+
+    @intent_handler(IntentBuilder("ConfuciusBirth").require('confucius').require('birth'))
+    def handle_birth(self, message):
+        utterance = self.dialog_renderer.render("birth", {})
+        self.show_confucius(utterance)
+        self.speak(utterance, wait=True)
+        self.gui.release()
+
+    @intent_handler(IntentBuilder("ConfuciusDeath").require('confucius').require('death'))
+    def handle_death(self, message):
+        utterance = self.dialog_renderer.render("death", {})
+        self.show_confucius(utterance)
+        self.speak(utterance, wait=True)
+        self.gui.release()
+
+    @intent_handler("who.intent")
+    def handle_who(self, message):
+        utterance = self.dialog_renderer.render("confucius", {})
+        self.show_confucius(utterance)
+        self.speak(utterance, wait=True)
+        self.gui.release()
